aa94fa4f6eafdb4d70126e95b05690902890cdb8
funtubxai <dpradhas12@gmail.com>
2026-03-04 14:02:39 +0530

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -1,8 +1,23 @@
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "AI Function Working Successfully 🚀"
-    }),
-  };
+import OpenAI from "openai";
+
+export async function handler(event){
+
+const {prompt}=JSON.parse(event.body);
+
+const openai=new OpenAI({
+apiKey:process.env.OPENAI_API_KEY
+});
+
+const result=await openai.images.generate({
+model:"gpt-image-1",
+prompt:prompt,
+size:"1024x1024",
+n:4
+});
+
+return{
+statusCode:200,
+body:JSON.stringify(result)
 };
+
+}
